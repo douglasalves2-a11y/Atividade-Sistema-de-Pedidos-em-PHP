24e248ee516ea91c707dfc3ed6f845c6f15a5ed2
Douglas H. Alves <douglas.alves2@alunos.unis.edu.br>
2026-04-11 18:15:22 -0300

    Add files via upload

---
+++ Produto.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+class Produto {
+    private string $nome;
+    private float $preco;
+
+    public function __construct(string $nome, float $preco) {
+        $this->nome = $nome;
+        $this->preco = $preco;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function getPreco(): float {
+        return $this->preco;
+    }
+
+    public function setNome(string $nome): void {
+        $this->nome = $nome;
+    }
+
+    public function setPreco(float $preco): void {
+        $this->preco = $preco;
+    }
+}
